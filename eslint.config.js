f167703b4d0ba8230b576c39d711f514a2ee2d0b
hello-larin <larinalesha@mail.ru>
2025-03-03 00:52:08 +0300

    Настроил конфиг eslint. Теперь не ругается на бандлер и на compiled.js

--- eslint.config.js
+++ eslint.config.js
@@ -4,9 +4,9 @@ import pluginJs from "@eslint/js";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
+  { ignores: ["**/dist/*", "**/src/compiled.js"] },
   {
-    languageOptions: { globals: globals.browser },
-    ignores: ["/src/compiled.js"]
+    languageOptions: { globals: globals.browser }
   },
   pluginJs.configs.recommended,
 ];
